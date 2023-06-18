0c3c25d115f3780acdbdd9c7bd7af3480e726791
Chirran <swainchittaranjan345@gmail.com>
2023-06-18 15:42:25 +0530

    Search functionality completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import compare from "../images/compare.svg";
 import wishlist from "../images/wishlist.svg";
@@ -7,23 +7,43 @@ import user from "../images/user.svg";
 import cart from "../images/cart.svg";
 import menu from "../images/menu.svg";
 import { useDispatch, useSelector } from "react-redux";
+import { Typeahead } from "react-bootstrap-typeahead";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+
 const Header = () => {
   const dispatch = useDispatch();
-  const cartState = useSelector(state => state?.auth?.cartProducts);
-  const authState = useSelector(state => state?.auth);
-  const [total,setTotal] = useState(null);
-  useEffect(()=>{
-    let sum =0;
-    for(let index=0;index<cartState?.length;index++){
-      sum = sum + (Number(cartState[index]?.quantity) * Number(cartState[index].price));
+  const navigate = useNavigate();
+  const cartState = useSelector((state) => state?.auth?.cartProducts);
+  const authState = useSelector((state) => state?.auth);
+  const productState = useSelector(state => state?.product?.product);
+  const [productOpt,setProductOpt] = useState([]);
+
+  const [total, setTotal] = useState(null);
+  const [paginate, setPaginate] = useState(true);
+
+  useEffect(() => {
+    let sum = 0;
+    for (let index = 0; index < cartState?.length; index++) {
+      sum =
+        sum +
+        Number(cartState[index]?.quantity) * Number(cartState[index].price);
     }
     setTotal(sum);
-  },[cartState])
+  }, [cartState]);
 
-  const handleLogout = ()=>{
+  useEffect(()=>{
+    let data=[];
+    for(let index=0;index<productState.length;index++){
+      const element = productState[index];
+      data.push({id:index, prod:element?._id,name:element?.title});
+    }
+    setProductOpt(data);
+  },[productState]);
+
+  const handleLogout = () => {
     localStorage.clear();
     window.location.reload();
-  }
+  };
 
   return (
     <>
@@ -49,19 +69,26 @@ const Header = () => {
       <header className="header-upper py-3">
         <div className="container-xxl">
           <div className="row align-items-center">
-            <div className="col-3" >
+            <div className="col-3">
               <h2>
-                <Link className="text-white" to="/">Bindaas Bazaar</Link>
+                <Link className="text-white" to="/">
+                  Bindaas Bazaar
+                </Link>
               </h2>
             </div>
             <div className="col-4">
               <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control py-2"
-                  placeholder="Search Product Here..."
-                  aria-label="Search Product Here..."
-                  aria-describedby="basic-addon2"
+                <Typeahead
+                  id="pagination-example"
+                  onPaginate={() => console.log("Results paginated")}
+                  onChange={(selected)=>{
+                    navigate(`/product/${selected[0].prod}`)
+                  }}
+                  options={productOpt}
+                  paginate={paginate}
+                  labelKey={"name"}
+                  minLength={2}
+                  placeholder="Search for products here..."
                 />
                 <span className="input-group-text p-3" id="basic-addon2">
                   <BsSearch className="fs-6" />
@@ -94,17 +121,19 @@ const Header = () => {
                 </div>
                 <div>
                   <Link
-                    to={authState?.user === null? "/login":"/my-profile"}
+                    to={authState?.user === null ? "/login" : "/my-profile"}
                     className="d-flex align-items-center gap-10 text-white"
                   >
                     <img src={user} alt="user" />
-                    {
-                      authState?.user === null? <p className="mb-0">
-                      Log in <br /> My Account
-                    </p> : <p className="mb-0">
-                      Welcome {authState?.user?.firstname}
-                    </p>
-                    }
+                    {authState?.user === null ? (
+                      <p className="mb-0">
+                        Log in <br /> My Account
+                      </p>
+                    ) : (
+                      <p className="mb-0">
+                        Welcome {authState?.user?.firstname}
+                      </p>
+                    )}
                   </Link>
                 </div>
                 <div>
@@ -114,8 +143,10 @@ const Header = () => {
                   >
                     <img src={cart} alt="cart" />
                     <div className="d-flex flex-column gap-10">
-                      <span className="badge bg-white text-dark">{cartState?.length? cartState.length:0}</span>
-                      <p className="mb-0">$ {total? total:0}</p>
+                      <span className="badge bg-white text-dark">
+                        {cartState?.length ? cartState.length : 0}
+                      </span>
+                      <p className="mb-0">$ {total ? total : 0}</p>
                     </div>
                   </Link>
                 </div>
@@ -172,7 +203,13 @@ const Header = () => {
                     <NavLink to="/my-orders">My Orders</NavLink>
                     <NavLink to="/blogs">Blogs</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
-                    <button onClick={handleLogout} className="border border-0 bg-transparent text-white text-uppercase" type="button">Logout</button>
+                    <button
+                      onClick={handleLogout}
+                      className="border border-0 bg-transparent text-white text-uppercase"
+                      type="button"
+                    >
+                      Logout
+                    </button>
                   </div>
                 </div>
               </div>
