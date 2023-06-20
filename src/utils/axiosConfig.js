bb5d884cbee007f3a06f5fe8a5b8046b896d1756
Chirran <swainchittaranjan345@gmail.com>
2023-06-20 12:33:40 +0530

    updated base_url

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,7 @@
-export const base_url = "http://localhost:5000/api/";
+// export const base_url = "http://localhost:5000/api/";
+export const base_url = "https://clicky-zoff.onrender.com/api/";
+
+
 const getTokenFromLocalStorage = localStorage.getItem("customer") ? JSON.parse(localStorage.getItem("customer")):null;
 
 export const config = {
