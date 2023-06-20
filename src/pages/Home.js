aceff1da78a3d424ef5d53ad59e865cbaa5ce252
Chirran <swainchittaranjan345@gmail.com>
2023-06-20 13:01:19 +0530

    cart updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,11 +48,13 @@ const Home = () => {
           <div className="col-6">
             <div className="main-banner position-relative ">
               <img
-                src="images/main-banner-1.jpg"
+                // src="images/main-banner-1.jpg"
+                 src="images/main-banner-2.jpg"
                 className="img-fluid rounded-3"
                 alt="main banner"
+                
               />
-              <div className="main-banner-content position-absolute">
+              <div className="main-banner-content position-absolute " >
                 <h4>SUPERCHARGED FOR PROS.</h4>
                 <h5>iPad S13+ Pro.</h5>
                 <p>From $999.00 or $41.62/mo.</p>
